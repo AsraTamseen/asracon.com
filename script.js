415ded5cb2344185813289ae55a4db213c977d14
AsraTamseen <83475846+AsraTamseen@users.noreply.github.com>
2021-10-19 21:33:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -16,6 +16,21 @@ $(document).ready(function(){
       $('.top').hide();
     }
 
+    $('a[href*="#"]').on('click',function(e){
+
+    e.preventDefault();
+
+    $('html, body').animate({
+
+      scrollTop : $($(this).attr('href')).offset().top,
+
+    },
+      500, 
+      'linear'
+    );
+
+  });
+    
   });
 
   var canvas = document.querySelector("#canvas"),
